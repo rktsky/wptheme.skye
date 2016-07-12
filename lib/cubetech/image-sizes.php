54d3695076ca1300e584e1d2454dc71b81e72faf
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2016-07-12 19:31:55 +0200

    commented out example

--- lib/cubetech/image-sizes.php
+++ lib/cubetech/image-sizes.php
@@ -6,4 +6,4 @@
 	 * Syntax: add_image_size( IMAGE_SLUG, MAX_WIDTH, MAX_HEIGHT, CROP_BOOL );
 	 */
 
-	add_image_size( 'ct-reference-overview-example', 1200, 800, false );
+	// add_image_size( 'ct-reference-overview-example', 1200, 800, false );
