4484f209b1ffd962f3bbf491916e140f8a201071
Christoph S. Ackermann <info@acki.be>
2018-09-14 14:21:11 +0200

    Adding option for --fast

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,9 @@ var enabled = {
   // Fail due to JSHint warnings only when `--production`
   failJSHint: argv.production,
   // Strip debug statments from javascript when `--production`
-  stripJSDebug: argv.production
+  stripJSDebug: argv.production,
+  // fast arg
+  minify: !argv.fast
 };
 
 // Path to the compiled assets manifest in the dist directory
@@ -110,8 +112,10 @@ var cssTasks = function(filename) {
         'opera 12'
       ]
     })
-    .pipe(cssNano, {
-      safe: true
+    .pipe(function() {
+	    return gulpif(enabled.minify, cssNano({
+			safe: true
+    	}));
     })
     .pipe(function() {
       return gulpif(enabled.rev, rev());
@@ -136,10 +140,12 @@ var jsTasks = function(filename) {
       return gulpif(enabled.maps, sourcemaps.init());
     })
     .pipe(concat, filename)
-    .pipe(uglify, {
-      compress: {
-        'drop_debugger': enabled.stripJSDebug
-      }
+    .pipe(function() {
+    	return gulpif(enabled.minify, uglify({
+			compress: {
+				'drop_debugger': enabled.stripJSDebug
+    		}
+    	}));
     })
     .pipe(function() {
       return gulpif(enabled.rev, rev());
@@ -220,14 +226,14 @@ gulp.task('fonts', function() {
 // `gulp images` - Run lossless compression on all the images.
 gulp.task('images', function() {
   return gulp.src(globs.images)
-    .pipe(imagemin([
+    .pipe(gulpif(enabled.minify, imagemin([
       imagemin.jpegtran({progressive: true}),
       imagemin.gifsicle({interlaced: true}),
       imagemin.svgo({plugins: [
         {removeUnknownsAndDefaults: false},
         {cleanupIDs: false}
       ]})
-    ]))
+    ])))
     .pipe(gulp.dest(path.dist + 'images'))
     .pipe(browserSync.stream());
 });
