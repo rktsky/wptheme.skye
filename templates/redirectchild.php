f5ffa0422ba0b095065d1106c014e774b87dd0fc
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2025-06-16 01:29:22 +0200

    a lot of fixes and removed a lot of ct sh!t

--- templates/redirectchild.php
+++ templates/redirectchild.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Weiterleitung an erste Unterseite
  *
- * Redirect functionality is via Hook in lib/cubetech/rediretchild.php
+ * Redirect functionality is via Hook in lib/rocketsky/rediretchild.php
  * because it needs to happen before wp_head is parsed.
  *
  * Since 30.5.2017 sva/mhm
